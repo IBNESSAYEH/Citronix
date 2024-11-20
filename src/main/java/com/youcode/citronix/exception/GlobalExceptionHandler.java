a5b6f51c0cf43a4bc711015a90bd4fd0a13949b2
abdellatif ibnessayeh <abdellatifibnessayeh@gmail.com>
2024-11-20 11:18:37 +0100

    add argumentException method

--- src/main/java/com/youcode/citronix/exception/GlobalExceptionHandler.java
+++ src/main/java/com/youcode/citronix/exception/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package com.youcode.citronix.exception;
 
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.youcode.citronix.exception.enums.ErrorMessages;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -22,5 +25,16 @@ public class GlobalExceptionHandler {
         ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
 
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value= MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> HandleMethodArgumentNotValid(MethodArgumentNotValidException ex, WebRequest request)
+    {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
